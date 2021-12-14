75b89c7eb76efb600871f98a8cffb47eab698559
saim <ahmad.saim9@gmail.com>
2021-12-15 02:19:22 +0530

    header design

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  menuItems: string[] = ['Home','About Us', 'Careers','Blog','Contact Us']
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
